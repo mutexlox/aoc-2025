5821b8796ea923d590dbb0e5686775353545ab5a
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2025-12-11 22:20:54 -0500

    do day 05 pt 1

---
+++ src/day05.rs
@@ -0,0 +1,38 @@
+use aoc_2025::util;
+use std::ops::RangeInclusive;
+
+fn count_fresh(ranges: &[RangeInclusive<i64>], nums: &[i64]) -> usize {
+    let mut count = 0;
+    for n in nums {
+        let mut works = false;
+        for r in ranges {
+            if r.contains(n) {
+                works = true;
+                break;
+            }
+        }
+        if works {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn main() {
+    let mut ranges = Vec::new();
+    let mut nums = Vec::new();
+    let mut done_ranges = false;
+    for line in util::get_lines().map_while(Result::ok) {
+        if !done_ranges {
+            if line.is_empty() {
+                done_ranges = true;
+                continue;
+            }
+            let (l, r) = line.split_once('-').unwrap();
+            ranges.push(l.parse::<i64>().unwrap()..=r.parse::<i64>().unwrap());
+        } else {
+            nums.push(line.parse::<i64>().unwrap());
+        }
+    }
+    println!("{}", count_fresh(&ranges, &nums));
+}
