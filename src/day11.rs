0fb0f49cd4990926adb68fd53de0497602df84d9
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2025-12-13 23:37:29 -0500

    do day 11 pt 2

--- src/day11.rs
+++ src/day11.rs
@@ -2,8 +2,6 @@ use aoc_2025::util;
 use std::collections::HashMap;
 
 fn paths(g: &HashMap<String, Vec<String>>, start: &str, end: &str) -> usize {
-    let mut cache = HashMap::new();
-
     fn paths_helper(
         g: &HashMap<String, Vec<String>>,
         start: &str,
@@ -24,7 +22,61 @@ fn paths(g: &HashMap<String, Vec<String>>, start: &str, end: &str) -> usize {
         cache[start]
     }
 
-    paths_helper(g, start, end, &mut cache)
+    paths_helper(g, start, end, &mut HashMap::new())
+}
+
+fn paths_through(
+    g: &HashMap<String, Vec<String>>,
+    start: &str,
+    end: &str,
+    must_visit: (&str, &str),
+) -> usize {
+    fn paths_helper(
+        g: &HashMap<String, Vec<String>>,
+        start: &str,
+        end: &str,
+        must_visit: (&str, &str),
+        saw_must_visit: (bool, bool),
+        cache: &mut HashMap<(String, (bool, bool)), usize>,
+    ) -> usize {
+        if start == end {
+            if saw_must_visit.0 && saw_must_visit.1 {
+                return 1;
+            }
+            return 0;
+        }
+        if let Some(&n) = cache.get(&(start.to_string(), saw_must_visit)) {
+            return n;
+        }
+
+        let sum = g[start]
+            .iter()
+            .map(|node| {
+                paths_helper(
+                    g,
+                    node,
+                    end,
+                    must_visit,
+                    (
+                        saw_must_visit.0 || node == must_visit.0,
+                        saw_must_visit.1 || node == must_visit.1,
+                    ),
+                    cache,
+                )
+            })
+            .sum();
+        cache.insert((start.to_string(), saw_must_visit), sum);
+        sum
+    }
+
+    paths_helper(
+        g,
+        start,
+        end,
+        must_visit,
+        (false, false),
+        &mut HashMap::new(),
+    )
 }
 
 fn main() {
@@ -38,4 +90,5 @@ fn main() {
         g.insert(node.to_string(), dests);
     }
     println!("{}", paths(&g, "you", "out"));
+    println!("{}", paths_through(&g, "svr", "out", ("dac", "fft")));
 }
